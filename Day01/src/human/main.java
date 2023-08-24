9aba2f6bd4454080972b7884b92fc55012cac7a4
jieun <jieun98.dev@gmail.com>
2023-08-24 11:06:49 +0900

    [mod] 생성자

---
+++ Day01/src/human/main.java
@@ -0,0 +1,13 @@
+package human;
+
+public class main {
+	public static void main(String[] args) {
+		Human human1 = new Human(20, "Hong gildong");
+		human1.speak();
+		
+		Student student1 = new Student();
+		student1.speak();
+		student1.learn();
+		
+	}
+}
