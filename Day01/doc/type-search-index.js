c1309a9a0177b5d2f4943fea57ebdba387c5228f
jieun <jieun98.dev@gmail.com>
2023-08-24 15:18:52 +0900

    [add] doc files

---
+++ Day01/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"zoo","l":"Animal"},{"p":"zoo","l":"Bird"},{"p":"zoo","l":"Chicken"},{"p":"zoo","l":"Flyable"},{"p":"zoo","l":"main"},{"p":"zoo","l":"Sparrow"}];updateSearchResults();
