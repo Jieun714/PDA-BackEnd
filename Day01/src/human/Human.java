60a68ddc899d63680dc0fb3c06d352789ebe404c
jieun <jieun98.dev@gmail.com>
2023-08-24 11:16:54 +0900

    [mod] 인자가 있는 생성자 추가

--- Day01/src/human/Human.java
+++ Day01/src/human/Human.java
@@ -4,6 +4,14 @@ public class Human {
 	int age;
 	String name;
 	
+	public Human() {} //기본 생성자 
+	
+	public Human(int age, String name) {
+		super();
+		this.age = age;
+		this.name = name;
+	}
+
 	public void speak() {
 		System.out.println("human speak()");
 	}
