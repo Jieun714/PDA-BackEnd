b7541497aa0a0a9b0068c4b6fb705535a1e7d855
jieun <jieun98.dev@gmail.com>
2023-08-24 10:47:30 +0900

    [add] Tutor.java

---
+++ human/src/human/Tutor.java
@@ -0,0 +1,14 @@
+package human;
+
+public class Tutor extends Human {
+	int age;
+	String name;
+	
+	public void speak() {
+		System.out.println("I am a Tutor");
+	}
+	
+	public void teach() {
+		System.out.println("I am teaching Java");
+	}
+}
