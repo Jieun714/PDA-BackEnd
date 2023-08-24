e9cbd3115a1a65ea59b7627b455931b7038c26c5
jieun <jieun98.dev@gmail.com>
2023-08-24 10:29:34 +0900

    [add] human java project

---
+++ human/src/human/human.java
@@ -0,0 +1,10 @@
+package human;
+
+public class human {
+	int age;
+	String name;
+	
+	public speak() {
+		System.out.println();
+	}
+}
---
+++ human/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ijieun
+ *
+ */
+module human {
+}
