b3824637b77fe917a417d5db1129602e40cef685
jieun <jieun98.dev@gmail.com>
2023-08-28 09:49:59 +0900

    [add] CharDisplay

---
+++ Day03/src/TemplateMethod/CharDisplay.java
@@ -0,0 +1,25 @@
+package TemplateMethod;
+
+public class CharDisplay extends AbstractDisplay {
+	private char ch; //표시해야되는 문자 
+	
+	public CharDisplay(char ch) {
+		this.ch = ch;
+	}
+	
+	@Override
+	public void open() {
+		System.out.print("<<");
+	}
+
+	@Override
+	public void print() {
+		System.out.print(ch);
+	}
+
+	@Override
+	public void close() {
+		System.out.println(">>");
+	}
+
+}
