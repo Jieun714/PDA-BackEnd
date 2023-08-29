dff87edf792d865ca83b6db20d00ab475184b19a
jieun <jieun98.dev@gmail.com>
2023-08-29 16:26:23 +0900

    [add] CampingCar

---
+++ Day04/src/Factory/CampingCar.java
@@ -0,0 +1,16 @@
+package Factory;
+
+/**
+ * Vehicle 인터페이스를 상속받는 클래스 
+ * */
+public class CampingCar implements Vehicle{
+	
+	/**
+	 * 인터페이스의 메소드를 Override한 메소드 
+	 * CampingCar 클래스의 메소드임을 확인하기 위해 시스템 출력  
+	 * */
+	@Override
+	public void startEngine() {
+		System.out.println("started simple engine of Camping Car...");
+	}
+}
