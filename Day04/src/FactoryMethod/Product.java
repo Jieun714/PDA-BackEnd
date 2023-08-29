da78fc33d2362c4d8a6fa5a0c59fdff1f0c69b32
jieun <jieun98.dev@gmail.com>
2023-08-29 13:40:19 +0900

    [add] Product

---
+++ Day04/src/FactoryMethod/Product.java
@@ -0,0 +1,13 @@
+package FactoryMethod;
+
+/**
+ * 제품을 표현한 추상 클래스
+ * @author Jieun Lee
+ * @version 1.0
+ * */
+public abstract class Product {
+	/**
+	 * 하위 클래스에서 구체화할 추상 메소드  
+	 * */
+	public abstract void use();
+}
