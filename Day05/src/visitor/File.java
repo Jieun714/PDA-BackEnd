19a6cd599a67ef108b7546b9b4ccfcabe61e42e1
jieun <jieun98.dev@gmail.com>
2023-08-30 16:43:36 +0900

    [add]

---
+++ Day05/src/visitor/File.java
@@ -0,0 +1,30 @@
+package visitor;
+
+/**
+ * 파일을 나타내는 클래스 
+ * */
+public class File extends Entry {
+	private String name;
+	private int size;
+	
+	public File(String name, int size) {
+		this.name = name;
+		this.size = size;
+	}
+
+	@Override
+	public void accept(Visitor v) {
+		v.visit(this);
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public int getSize() {
+		return size;
+	}
+
+}
