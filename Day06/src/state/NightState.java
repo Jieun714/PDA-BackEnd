9cd26c4f19c02d2e24d9838feeef06fb4ff99335
jieun <jieun98.dev@gmail.com>
2023-08-31 13:57:28 +0900

    [add] NightState

---
+++ Day06/src/state/NightState.java
@@ -0,0 +1,30 @@
+package state;
+
+public class NightState implements State {
+	private static NightState singleton = new NightState();
+	private NightState() {}
+	
+	public static State getInstance() {
+		return singleton;
+	}
+	
+	@Override
+	public void doUse(Context context) {
+		context.recordLog("비상: 야간 금고 사용!");
+	}
+
+	@Override
+	public void doAlarm(Context context) {
+		context.callSecurityCenter("비상벨(야간)");
+	}
+
+	@Override
+	public void doPhone(Context context) {
+		context.callSecurityCenter("야간 통화 녹음");
+	}
+
+	@Override
+	public String toString() {
+		return "[야간]";
+	}
+}
